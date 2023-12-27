51d088fd73c16c65f390b21c3a63510d9f93c7e3
ruanjanau <rhuanjanau531@gmail.com>
2023-12-27 12:30:36 -0400

    feat create entity

---
+++ lib/src/app/domain/entities/products_entity.dart
@@ -0,0 +1,15 @@
+class ProductsEntity {
+  final int id;
+  final String product;
+  final int price;
+  final String image;
+  final String details;
+
+  ProductsEntity({
+    required this.id,
+    required this.product,
+    required this.price,
+    required this.image,
+    required this.details,
+  });
+}

727efa91736ca62fc796ad7ae4d6ae3e697953c0
ruanjanau <rhuanjanau531@gmail.com>
2023-12-29 04:19:12 -0400

    delete file

--- lib/src/app/products/bloc/products_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'products_bloc.dart';
-
-@freezed
-class ProductsEvent with _$ProductsEvent {
-  const factory ProductsEvent.findAll() = _ProductsEventFindAll;
-}
--- lib/src/app/products/bloc/products_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'products_bloc.dart';
-
-@freezed
-class ProductsState with _$ProductsState {
-  const factory ProductsState.initial() = _ProductsStateInitial;
-}

0c56171c0cf30bf6bf377aea7b88831a8dfed92f
ruanjanau <rhuanjanau531@gmail.com>
2023-12-29 18:56:42 -0400

    alter in routes

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
-import 'app/splash/view/splash.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 import 'app/login/view/login.dart';
+import 'app/products/data/datasource/datasource.dart';
+import 'app/products/presentation/view/bloc/products_bloc.dart';
 import 'app/products/presentation/view/view.dart';
+import 'app/splash/view/splash.dart';
 
 class Routes extends StatelessWidget {
   const Routes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (_) => ProductsDataSource(Dio()),
+        ),
+      ],
+      child: MaterialApp(
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/login': (context) => LoginPage(),
+          '/products': (context) => BlocProvider(
+                create: (context) =>
+                    ProductsBloc(dataSource: context.read<ProductsDataSource>())
+                      ..add(const ProductsEvent.finAll()),
+                child: const ProductsPage(),
+              ),
+        },
       ),
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => LoginPage(),
-        '/products': (context) => const ProductsPage(),
-      },
     );
   }
 }

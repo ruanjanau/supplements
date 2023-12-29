5a49cb7d7ab39821b4a1beeac6f2dcbcf9fc839c
ruanjanau <rhuanjanau531@gmail.com>
2023-12-29 18:56:35 -0400

    feat create details products

---
+++ lib/src/app/products/presentation/view/details/products_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductsDetail extends StatelessWidget {
+  const ProductsDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalhes do Produto'),
+      ),
+      body: Container(),
+    );
+  }
+}

cd7bc26a38f7c0e15b67dc6de5974b6ae470f579
ruanjanau <rhuanjanau531@gmail.com>
2023-12-29 04:17:19 -0400

    alter main

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'src/app/login/view/login.dart';
-import 'src/app/products/presentation/view/view.dart';
-import 'src/app/splash/view/splash_screen.dart';
+
+import 'src/routes.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      // initialRoute: '/',
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => LoginPage(),
-        '/products': (context) => const ProductsPage(),
-      },
-    );
-  }
+  runApp(const Routes());
 }

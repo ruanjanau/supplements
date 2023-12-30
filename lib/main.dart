00dd4b16efa9f29528e182b01d23180a4c485576
ruanjanau <rhuanjanau531@gmail.com>
2023-12-30 17:06:59 -0400

    alter routes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
-import 'src/routes.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'src/app/login/view/login.dart';
+import 'src/app/products/data/datasource/datasource.dart';
+import 'src/app/products/presentation/view/bloc/products_bloc.dart';
+import 'src/app/products/presentation/view/view.dart';
+import 'src/app/splash/view/splash.dart';
 
 void main() {
-  runApp(const Routes());
+  runApp(
+    RepositoryProvider(
+      create: (context) => ProductsDataSource(Dio()),
+      child: MaterialApp(
+        initialRoute: 'login',
+        routes: {
+          '/': (_) => const SplashScreen(),
+          '/login': (_) => LoginPage(),
+          '/bloc/products/': (context) => BlocProvider(
+                create: (_) =>
+                    ProductsBloc(dataSource: context.read<ProductsDataSource>())
+                      ..add(const ProductsEvent.findAll()),
+                child: const ProductsPage(),
+              ),
+        },
+      ),
+    ),
+    // ),
+  );
 }

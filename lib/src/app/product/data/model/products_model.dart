c1d0daa71f6b5642917f59aebfcfc27039015618
ruanjanau <rhuanjanau531@gmail.com>
2024-01-06 16:56:02 -0400

    alter model

--- lib/src/app/product/data/model/products_model.dart
+++ lib/src/app/product/data/model/products_model.dart
@@ -4,15 +4,19 @@ class ProductsModel {
   final int? id;
   final String? product;
   final String? price;
+  final String? brand;
   final String? image;
   final String? details;
+  final bool? stock;
 
   ProductsModel({
     this.id,
     this.product,
     this.price,
+    this.brand,
     this.image,
     this.details,
+    this.stock,
   });
 
   factory ProductsModel.fromJson(dynamic map) {
@@ -20,8 +24,10 @@ class ProductsModel {
       id: map['id']?.toInt(),
       product: map['product'] ?? '',
       price: map['price'] ?? '',
+      brand: map['brand'] ?? '',
       image: map['image'] ?? '',
       details: map['details'] ?? '',
+      stock: map['stock'] ?? false,
     );
   }
 
@@ -30,8 +36,10 @@ class ProductsModel {
       id: id ?? 0,
       product: product ?? '',
       price: price ?? '',
+      brand: brand ?? '',
       image: image ?? '',
       details: details ?? '',
+      stock: stock ?? false,
     );
   }
 }
--- lib/src/app/product/presentation/components/card_products.dart
+++ lib/src/app/product/presentation/components/card_products.dart
@@ -1,84 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class CardProducts extends StatelessWidget {
   final String image;
   final String product;
   final String price;
+  final String brand;
 
   const CardProducts({
     Key? key,
     required this.image,
     required this.product,
     required this.price,
+    required this.brand,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Card(
-          child: Stack(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        width: 190,
+        height: 245,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 10,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 240.0,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    child: Image.network(
-                      image,
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-                  Text(
-                    product,
-                    style: const TextStyle(
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        child: Text(
-                          price,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w800,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8.0)
-                ],
-              ),
-              Positioned(
-                top: 0.0,
-                right: 0.0,
-                child: IconButton(
-                  iconSize: 34,
-                  icon: const Icon(
-                    Icons.favorite_border,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {
-                    //TODO: aply logic for favorites
-                  },
+              const SizedBox(height: 8),
+              Center(
+                child: Image.network(
+                  image,
+                  height: 110,
                 ),
+              ),
+              Text(
+                product,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                brand,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 6),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    price,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
-      ],
+      ).animate().slide().fade(),
     );
   }
 }
